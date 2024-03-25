6fe1dea54f42208161bfe92d730af78f67f67652
DiroGL <114400754+DiroGL@users.noreply.github.com>
2024-03-25 12:30:57 +0100

    Configuracion Inicial
    Proyecto Nuevo

---
+++ Fronted-climclip/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Fronted-climclip',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
