b95f5d0166ad351dd35bbf9158a7a888199f83a9
DiroGL <114400754+DiroGL@users.noreply.github.com>
2024-05-30 16:17:34 +0200

    cambios firebase y paquete

--- Fronted-climclip/capacitor.config.ts
+++ Fronted-climclip/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Fronted-climclip',
+  appId: 'io.chalknity.manotas',
+  appName: 'Chalknity',
   webDir: 'www',
   server: {
     androidScheme: 'https'
